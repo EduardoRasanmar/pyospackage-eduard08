7c1426e7f46f6e655a989f5728a02389349240e8
Eduardo Sanchez <ed.rasanmar@gmail.com>
2026-01-08 13:44:26 -0800

    my package

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# pyospackage-eduardo08
+# Welcome to pyospackage-eduardo08
+
+|        |        |
+|--------|--------|
+| Package | [![Latest PyPI Version](https://img.shields.io/pypi/v/pyospackage-eduardo08.svg)](https://pypi.org/project/pyospackage-eduardo08/) [![Supported Python Versions](https://img.shields.io/pypi/pyversions/pyospackage-eduardo08.svg)](https://pypi.org/project/pyospackage-eduardo08/)  |
+| Meta   | [![Code of Conduct](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](CODE_OF_CONDUCT.md) |
+
+*TODO: the above badges that indicate python version and package version will only work if your package is on PyPI.
+If you don't plan to publish to PyPI, you can remove them.*
+
+pyospackage-eduardo08 is a project that (describe what it does here).
+
+## Get started
+
+You can install this package into your preferred Python environment using pip:
+
+```bash
+$ pip install pyospackage-eduardo08
+```
+
+TODO: Add a brief example of how to use the package to this section
+
+To use pyospackage-eduardo08 in your code:
+
+```python
+>>> import pyospackage-eduardo08
+>>> pyospackage-eduardo08.hello_world()
+```
+
+## Copyright
+
+- Copyright © 2026 Eduardo Sanchez.
+- Free software distributed under the [MIT License](./LICENSE).
---
+++ src/pyospackage_eduardo08/example.py
@@ -0,0 +1,68 @@
+"""
+A module that adds numbers together.
+
+You may want to delete this module or modify it for your package.
+It's generally good practice to have a docstring
+that explains the purpose of the module, at the top.
+"""
+
+def add_numbers(a, b):
+    """
+    Add two numbers together and return the result.
+
+    This is an example function with a numpy style docstring.
+    We recommend using this style for consistency and readability.
+
+    Parameters
+    ----------
+    a : float
+        The first number to add.
+    b : float
+        The second number to add.
+
+    Returns
+    -------
+    float
+        The sum of the two numbers.
+
+    Examples
+    --------
+    >>> add_numbers(3, 5)
+    8
+    >>> add_numbers(-2, 7)
+    5
+
+    """
+    return a + b
+
+
+def substract_numbers(a, b):
+    """
+    Substract two numbers together and return the result.
+
+    This is an example function with a numpy style docstring.
+    We recommend using this style for consistency and readability.
+
+    Parameters
+    ----------
+    a : float
+        The first number to substract.
+    b : float
+        The second number to substract.
+
+    Returns
+    -------
+    float
+        The substraction of the two numbers.
+
+    Examples
+    --------
+    >>> substract_numbers(10, 5)
+    5
+    >>> substract_numbers(-2, 7)
+    -9
+
+    """
+    return a - b
+
+
